45cf8013d9530f4fc78d45509ec812e471f19bd0
abdessalem1998 <89970442+abdessalem1998@users.noreply.github.com>
2021-12-07 16:31:09 +0100

    Set an array of some simple to do tasks (array of objects)

--- src/index.js
+++ src/index.js
@@ -1,6 +1,26 @@
 import _ from 'lodash';
 import './style.css';
 
+const tasks = [
+  {
+    description: 'task 1 description',
+    completed: true,
+    index: 0,
+  },
+  {
+    description: 'task 2 description',
+    completed: false,
+    index: 1,
+  },
+  {
+    description: 'task 3 description',
+    completed: true,
+    index: 2,
+  },
+];
+
+tasks[0] = {};
+
 function component() {
   const element = document.createElement('div');
 

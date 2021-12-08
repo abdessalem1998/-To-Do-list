3b818c28cf744d1d8ba837111cc94ec94db6477b
abdessalem1998 <89970442+abdessalem1998@users.noreply.github.com>
2021-12-08 10:46:00 +0100

    Render the dynamically created list of tasks in the dedicated placeholder in order of the index

--- src/index.js
+++ src/index.js
@@ -1,34 +1,48 @@
-import _ from 'lodash';
 import './style.css';
 
 const tasks = [
   {
     description: 'task 1 description',
     completed: true,
-    index: 0,
+    index: 2,
   },
   {
     description: 'task 2 description',
     completed: false,
-    index: 1,
+    index: 0,
   },
   {
     description: 'task 3 description',
     completed: true,
-    index: 2,
+    index: 1,
   },
 ];
 
-tasks[0] = {};
+// sort the array of tasks from small index to biger
+const sortArray = (array) => {
+  let temp = 0;
+  for (let i = 0; i < array.length; i += 1) {
+    for (let j = i; j < array.length; j += 1) {
+      if (array[j].index < array[i].index) {
+        temp = array[j];
+        array[j] = array[i];
+        array[i] = temp;
+      }
+    }
+  }
+  return array;
+};
 
-function component() {
-  const element = document.createElement('div');
+// display tasks
+const displayList = (arr) => {
+  sortArray(tasks);
+  const task = document.getElementById('list');
+  for (let i = 0; i < arr.length; i += 1) {
+    const element = document.createElement('li');
+    element.innerHTML = `${arr[i].description}`;
+    task.appendChild(element);
+  }
+  return task;
+};
 
-  // Lodash, now imported by this script
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element.classList.add('hello');
-
-  return element;
-}
-
-document.body.appendChild(component());
+document.body.appendChild(displayList(tasks));
